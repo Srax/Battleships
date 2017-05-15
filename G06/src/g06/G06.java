d76e6186bc13915343ef617408ddb04cb7688727
DD <DD@10.50.137.97>
2017-05-15 09:53:08 +0200

    Implemented interfaces

--- G06/src/g06/G06.java
+++ G06/src/g06/G06.java
@@ -5,17 +5,40 @@
  */
 package g06;
 
+import battleship.interfaces.BattleshipsPlayer;
+import tournament.player.PlayerFactory;
+
 /**
  *
  * @author thoma
  */
-public class G06 {
+public class G06 implements PlayerFactory<BattleshipsPlayer>{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+
+    @Override
+    public BattleshipsPlayer getNewInstance() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getID() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String[] getAuthors() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
